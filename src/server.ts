5c6adad6ee51cad066357ae5fe56a7e971e21db5
Sudarshan-T <60401950+Sudarshan-T@users.noreply.github.com>
2025-08-27 23:15:39 +0530

    Updated cors

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: [
-      "https://hotel-booking-system-ui.vercel.app",
+      "https://myvesta.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
